b72ea4a61a63837a36ec8547b73a6bb6e9a2a30b
ankit kumar <ankitsarwal7@gmail.com>
2025-01-01 14:51:56 +0530

    first proublem

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-const name = "ankit"
+//  1 => Program Question: Search and Count Students in an Array
+// Write a program that includes a list of student names. Implement a function that performs the following tasks:
+
+// Searches for a specific student's name in the array.
+// If the name is found, output a message indicating the name was found.
+// If the name is not found, output a message saying it was not found.
+
+
+ 
+// const StudentName = ['ankit', 'amit', 'sumit', 'rohit']
+
+// function findstudent (allStudent, studentName) {
+//      for(let i= 0; i < allStudent.length; i++) {
+//         if( allStudent[i] === studentName) {
+//             console.log(` found ${studentName}`)
+//         }
+//         else (!studentName)
+//         console.log("name not found ");
+        
+//      }
+ 
+// }
+// findstudent(StudentName, 'alex')
