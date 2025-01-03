6658a3662119c59759888cea099431e39101bc3f
ankitsarwal7 <ankitsarwal7@gmail.com>
2025-01-03 13:38:58 +0530

    Maximum Profit from Stock Prices

--- index.js
+++ index.js
@@ -186,4 +186,25 @@ const fizzBuzz = (n) =>  {
 }
 // fizzBuzz(15)
 
-//
+
+ //-------------------------------------------------------------------------
+
+ //  8 => Maximum Profit from Stock Prices
+
+
+function maxProfit(prices) {
+    let minPrice = Infinity
+    let maxProfit = 0
+
+for( let price of prices){
+    if( price < minPrice){
+        minPrice = price
+    }else {
+        maxProfit = Math.max(maxProfit, price - minPrice)
+    }
+}
+return maxProfit;
+}
+
+const prices = [7, 1, 5, 3, 6, 4];
+// console.log(maxProfit(prices));
